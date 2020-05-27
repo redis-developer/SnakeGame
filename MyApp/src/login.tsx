1f67d42d415e48e695970e53e4f89363217afff9
rachit2501 <rac.sri25@gmail.com>
2020-05-27 06:03:19 +0530

    testing and deployment left

--- MyApp/src/login.tsx
+++ MyApp/src/login.tsx
@@ -39,10 +39,53 @@ const Login: React.SFC<Props> = (props: Props) => {
     }
   };
 
+  const register = async () => {
+    const data = {username, password};
+    console.log(data);
+    try {
+      const response = await axios.post('http://10.0.3.2:3000/v1/create', data);
+      if (response.status === 200) {
+        props.update(
+          response.data.response.count,
+          response.data.response.username,
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <Container>
         <Header />
+        <Content>
+          <Text>Login</Text>
+          <Form>
+            <Item stackedLabel>
+              <Label>Username</Label>
+              <Input
+                value={username}
+                onChangeText={(e: any) => updateUsername(e)}
+              />
+            </Item>
+            <Item stackedLabel last>
+              <Label>Password</Label>
+              <Input
+                value={password}
+                onChangeText={(e: any) => updatePassword(e)}
+              />
+            </Item>
+            <Button bordered dark onPress={update}>
+              <Text>Enter</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+
+      <Container>
+        <Header />
+        <Text>Register</Text>
         <Content>
           <Form>
             <Item stackedLabel>
@@ -59,12 +102,12 @@ const Login: React.SFC<Props> = (props: Props) => {
                 onChangeText={(e: any) => updatePassword(e)}
               />
             </Item>
+            <Button bordered dark onPress={register}>
+              <Text>Register</Text>
+            </Button>
           </Form>
         </Content>
       </Container>
-      <Button bordered dark onPress={update}>
-        <Text>Enter</Text>
-      </Button>
     </>
   );
 };
